cab0b53effb02938ecb46d62148f0b2fdd8b4972
YuXiaoPan <peyppicp.pan@kikatech.com>
2017-12-15 01:56:50 +0800

    debug

--- src/main/java/me/peyppicp/cnn/EmojiSampleProcess.java
+++ src/main/java/me/peyppicp/cnn/EmojiSampleProcess.java
@@ -1,8 +1,29 @@
 package me.peyppicp.cnn;
 
+import com.google.common.collect.ArrayListMultimap;
+import com.vdurmont.emoji.EmojiParser;
+import me.peyppicp.advance2.WordToIndex;
+import org.apache.commons.io.Charsets;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
 public class EmojiSampleProcess {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        File file = new File("emoji_sample.txt");
+        WordToIndex wordToIndex = new WordToIndex(file.getCanonicalPath());
+        List<String> emojiSamples = FileUtils.readLines(file, Charsets.UTF_8);
+        Map<String, Integer> wordIndexMap = wordToIndex.getWordIndexMap();
+        ArrayListMultimap<String, String> labelToSamples = ArrayListMultimap.create(); //emoji -> samples
+        for (String sample : emojiSamples) {
+            List<String> emojis = EmojiParser.extractEmojis(sample);
+            if (!emojis.isEmpty()) {
 
+            }
+        }
     }
 }
