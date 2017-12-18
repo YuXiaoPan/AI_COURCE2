86d39ee76c0e48d65375e8fd02c22347ba5b0c4c
YuXiaoPan <peyppicp.pan@kikatech.com>
2017-12-18 17:24:59 +0800

    性能优化

--- src/main/java/me/peyppicp/cnn/RnnPredictEmoji.java
+++ src/main/java/me/peyppicp/cnn/RnnPredictEmoji.java
@@ -35,10 +35,10 @@ import java.util.Random;
  */
 public class RnnPredictEmoji {
 
-    //    public static final String OUTPUT = "/home/peyppicp/output/";
-    //    public static final String PREFIX = "/home/peyppicp/data/new/";
-    public static final String PREFIX = "/home/panyuxiao/data/new/";
-    public static final String OUTPUT = "/home/panyuxiao/output/";
+        public static final String OUTPUT = "/home/peyppicp/output/";
+        public static final String PREFIX = "/home/peyppicp/data/new/";
+//    public static final String PREFIX = "/home/panyuxiao/data/new/";
+//    public static final String OUTPUT = "/home/panyuxiao/output/";
 //    public static final String PREFIX = "";
 //    public static final String OUTPUT = "";
 
